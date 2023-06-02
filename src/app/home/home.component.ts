71f94dc1eadf67bc39d2c03cfdd5735f4b4b82b6
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-06-02 15:30:16 -0300

    mais uma outra atualizacao

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Anuncio } from '../anuncios';
+import { AnunciosService } from '../anuncios.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  anuncios: Anuncio[] = [];
 
+  constructor(private anunciosService: AnunciosService){}
+
+
+  ngOnInit(): void {
+    this.loadAnuncios();
+  }
+
+  loadAnuncios() {
+    this.anunciosService.getAnuncios().subscribe({
+      next: (data) => (this.anuncios = data),
+    });
+  }
 }
+
+
